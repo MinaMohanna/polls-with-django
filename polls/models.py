97ad40d3b37fbd55a84ea73a6d0776763d4e580f
MinaMohanna <minajoya68@gmail.com>
2026-09-22 04:03:24 +0330

    Rename question fields

--- polls/models.py
+++ polls/models.py
@@ -2,11 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
-    qestion_text = models.CharField(max_length=200)
-    pun_date = models.DateTimeField('date published')
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
 
 class choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text= models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    
